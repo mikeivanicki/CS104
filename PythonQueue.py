b8aba1d9f501cc4780069a533c1eec690ccb8bb1
mikeivanicki <91150796+mikeivanicki@users.noreply.github.com>
2021-11-28 18:28:04 -0500

    Python Bank Queue Assignment

---
+++ PythonQueue.py
@@ -0,0 +1,29 @@
+#Python Queue Simulation
+names = []
+for i in range(10):
+    names.append('Michael')
+    names.append ('Dave')
+    names.append ('James')
+    names.append ('Samantha')
+    names.append ('Matt')
+    names.append ('Darrell')
+    names.append ('Bruce')
+    names.append ('Vic')
+    names.append ('Jessica')
+    names.append ('Justin')
+for i in range(10):
+    len(names)
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+
+   
+    
+     
